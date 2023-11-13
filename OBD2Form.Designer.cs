bd4c929a9bfb90a30ede52c3cb2e9b1357616412
Lorenzo Cipelli <lorezocipelli@outlook.it>
2023-11-13 14:57:34 +0100

    thread safe display

--- OBD2Form.Designer.cs
+++ OBD2Form.Designer.cs
@@ -28,81 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            portTextBox = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            ipTextBox = new TextBox();
-            initConnButton = new Button();
-            display = new RichTextBox();
-            SuspendLayout();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.initConnButton = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.RichTextBox();
+            this.askPIDButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // portTextBox
             // 
-            portTextBox.Location = new Point(58, 41);
-            portTextBox.Name = "portTextBox";
-            portTextBox.Size = new Size(137, 23);
-            portTextBox.TabIndex = 0;
-            portTextBox.Text = "35000";
+            this.portTextBox.Location = new System.Drawing.Point(58, 41);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(137, 23);
+            this.portTextBox.TabIndex = 0;
+            this.portTextBox.Text = "35000";
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(2, 44);
-            label1.Name = "label1";
-            label1.Size = new Size(50, 15);
-            label1.TabIndex = 1;
-            label1.Text = "OBD2 IP";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OBD2 IP";
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(17, 15);
-            label2.Name = "label2";
-            label2.Size = new Size(35, 15);
-            label2.TabIndex = 2;
-            label2.Text = "PORT";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PORT";
             // 
             // ipTextBox
             // 
-            ipTextBox.Location = new Point(58, 12);
-            ipTextBox.Name = "ipTextBox";
-            ipTextBox.Size = new Size(137, 23);
-            ipTextBox.TabIndex = 3;
-            ipTextBox.Text = "192.168.0.149";
+            this.ipTextBox.Location = new System.Drawing.Point(58, 12);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(137, 23);
+            this.ipTextBox.TabIndex = 3;
+            this.ipTextBox.Text = "192.168.0.149";
             // 
             // initConnButton
             // 
-            initConnButton.Location = new Point(12, 70);
-            initConnButton.Name = "initConnButton";
-            initConnButton.Size = new Size(183, 23);
-            initConnButton.TabIndex = 4;
-            initConnButton.Text = "Init TCP Connection";
-            initConnButton.UseVisualStyleBackColor = true;
-            initConnButton.Click += button1_Click;
+            this.initConnButton.Location = new System.Drawing.Point(12, 70);
+            this.initConnButton.Name = "initConnButton";
+            this.initConnButton.Size = new System.Drawing.Size(183, 23);
+            this.initConnButton.TabIndex = 4;
+            this.initConnButton.Text = "Init TCP Connection";
+            this.initConnButton.UseVisualStyleBackColor = true;
+            this.initConnButton.Click += new System.EventHandler(this.initConnButton_Click);
             // 
             // display
             // 
-            display.Location = new Point(201, 12);
-            display.Name = "display";
-            display.Size = new Size(782, 81);
-            display.TabIndex = 5;
-            display.Text = "";
+            this.display.Location = new System.Drawing.Point(241, 12);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(500, 152);
+            this.display.TabIndex = 5;
+            this.display.Text = "";
+            // 
+            // askPIDButton
+            // 
+            this.askPIDButton.Location = new System.Drawing.Point(13, 101);
+            this.askPIDButton.Name = "askPIDButton";
+            this.askPIDButton.Size = new System.Drawing.Size(96, 23);
+            this.askPIDButton.TabIndex = 6;
+            this.askPIDButton.Text = "Ask PID";
+            this.askPIDButton.UseVisualStyleBackColor = true;
+            this.askPIDButton.Click += new System.EventHandler(this.askPIDButton_Click);
             // 
             // OBD2Form
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(995, 525);
-            Controls.Add(display);
-            Controls.Add(initConnButton);
-            Controls.Add(ipTextBox);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(portTextBox);
-            Name = "OBD2Form";
-            Text = "OBD II - Logger";
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 429);
+            this.Controls.Add(this.askPIDButton);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.initConnButton);
+            this.Controls.Add(this.ipTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portTextBox);
+            this.Name = "OBD2Form";
+            this.Text = "OBD II - Logger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
@@ -113,5 +126,6 @@
         private TextBox ipTextBox;
         private Button initConnButton;
         private RichTextBox display;
+        private Button askPIDButton;
     }
 }
--- OBD2Form.cs
+++ OBD2Form.cs
@@ -11,51 +11,81 @@ namespace OBD_II_WiFi
         TcpClient client = new TcpClient();
         NetworkStream stream;
         StreamReader reader;
-        byte[] data;
+        string data;
+
+        delegate void writeDisplayDelegate(string toDisplay);
 
         public OBD2Form()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void initConnButton_Click(object sender, EventArgs e)
         {
+            int n_try = 0;
             var ip_address = IPAddress.Parse(ipTextBox.Text); // parsing dell'indirizzo IP
-            var port = Int32.Parse(portTextBox.Text); // parsing della porta, per ODB2 sempre 35000
+            var port = Int32.Parse(portTextBox.Text); // parsing della porta, per ODB2 sempre 35000          
 
-            try
+            await Task.Run(() =>
             {
-                client.Connect(ip_address, port);
+                while (n_try < 5)
+                {
+                    try
+                    {
+                        writeDisplay("Trying to connect...");
+                        client.Connect(ip_address, port);
 
-                stream = client.GetStream();
-                reader = new StreamReader(stream, Encoding.ASCII);
-            }
-            catch (SocketException error)
-            {
-                display.Text = "Connection is NOT successfull, try again later! -> " + error.Message;
-            }
-            catch (Exception error)
-            {
-                display.Text = error.Message;
-            }
+                        stream = client.GetStream();
+                        reader = new StreamReader(stream, Encoding.ASCII);
+                    }
+                    catch (SocketException error)
+                    {
+                        n_try++;
+                        writeDisplay("Attempt #" + n_try + " " + error.Message);
+                    }
+                }
+            });
 
-            data = new byte[client.ReceiveBufferSize];
-
-            findOutPIDs();
-
-            /*Ping ping = new Ping();
-
-            PingReply result = await ping.SendPingAsync(ip_to_ping);
-
-            display.Text = "Device with IP: " + result.Address.ToString() + " pinged";
-            display.Text += "\nTTL: " + result.RoundtripTime.ToString();*/
-
-            //return result.Status == IPStatus.Success;
+            if (n_try < 5) startListening(); // apertura thread di ascolto della stream
+            else { display.Text += "\n" + "IP address is uncreachable at the moment, try again later..."; }
         }
 
+        private async void startListening() {
+            await Task.Run(() =>
+            {
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                byte[] mesajj = new byte[bytesRead];
+
+                for (int i = 0; i < bytesRead; i++)
+                {
+                    mesajj[i] = buffer[i];
+                }
+
+                data += Encoding.Default.GetString(mesajj, 0, bytesRead).Replace("\r", " ");
+                writeDisplay(data);
+            });
+        }
+
+        private void writeDisplay(string text) {
+            if (display.InvokeRequired)
+            {
+                writeDisplayDelegate d = new writeDisplayDelegate(writeDisplay);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                display.Text += "\n" + text;
+            }
+        }
+
+        private void askPIDButton_Click(object sender, EventArgs e)
+        {
+            findOutPIDs();
+        }
 
         void findOutPIDs() {
-            //send("0100" + "\r");
+            send("0100" + "\r");
             //send("0120" + "\r");
             //send("0140" + "\r");
             //send("0160" + "\r");
@@ -80,6 +110,18 @@ namespace OBD_II_WiFi
                 Console.WriteLine(e.Message);
             }
 
+        }
+
+        private async Task pingFunctionAsync() {
+            var ip_address = IPAddress.Parse(ipTextBox.Text);
+            Ping ping = new Ping();
+
+            PingReply result = await ping.SendPingAsync(ip_address);
+
+            display.Text = "Device with IP: " + result.Address.ToString() + " pinged";
+            display.Text += "\nTTL: " + result.RoundtripTime.ToString();
+
+            //return result.Status == IPStatus.Success;
         }
     }
 }
