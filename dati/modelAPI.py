97356735acd67015cdf26c7c1ca08d2c63bf52e7
Lorenzo Cipelli <lorezocipelli@outlook.it>
2023-12-18 13:36:57 +0100

    modifica API -> creazione risposta con most_common prediction

--- dati/modelAPI.py
+++ dati/modelAPI.py
@@ -3,10 +3,12 @@
 # - pip install joblib
 
 import os
-import uvicorn
-from fastapi import FastAPI
 import joblib
+import uvicorn
+
+from fastapi import FastAPI
 from pydantic import BaseModel
+from collections import Counter
 
 class CarData(BaseModel):
   rpm: int
@@ -23,6 +25,11 @@ joblib_in = open("dati/model/finalized_model.pkl","rb")
 model = joblib.load(joblib_in)
 
 predictions = []
+BUFFER_SIZE = 5
+
+def most_common(my_list):
+    data = Counter(my_list)
+    return data.most_common(1)[0][0]
 
 @app.get('/')
 def index():
@@ -35,31 +42,41 @@ def index():
 def predict_car_type(instance:CarData):
     instance = instance.model_dump()
 
-    rpm=instance['rpm']
-    maf=instance['maf']
-    iat=instance['iat']
-    speed=instance['speed']
-    engineload=instance['engineload']
-    
+    # qui prendo solamente le informazioni necessarie per la predizione
+    rpm = instance['rpm']
+    maf = instance['maf']
+    iat = instance['iat']
+    speed = instance['speed']
+    engineload = instance['engineload']
+
+    # il modello addestrato effettua la predizione
     pred = model.predict([[rpm, maf, iat, speed, engineload]])
+    predictions.append(pred)
 
-    #prediction.append(pred)
-
-    if pred == 1 :
+    if len(predictions) < BUFFER_SIZE :
+        predictions.append(pred)
         return {
-            'state': "OK",
-            'prediction': "SPORT"
-        }
-    elif pred == 2 :
-        return {
-            'state': "OK",
-            'prediction': "ECO"
-        }   
-    else :
-        return {
-            'state': "ERROR",
-            'message': "Cannot Predict"
-        }  
+            'state': "BUFFERING",
+            'message': "System needs more data.\nWait a few seconds!"
+        } 
+    else : # se abbiamo abbastanza predizioni nel buffer
+        final_pred = most_common(predictions) # prendo il valore con più ricorrenze
+        del predictions[0] # tolgo il primo elemento della lista per il giro successivo
+        if final_pred == 1 :
+            return {
+                'state': "OK",
+                'prediction': "SPORT"
+            }
+        elif final_pred == 2 :
+            return {
+                'state': "OK",
+                'prediction': "ECO"
+            }
+        else :
+            return {
+                'state': "ERROR",
+                'message': "Cannot Predict"
+            }
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
